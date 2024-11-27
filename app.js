1a31d705c7f381a367f0a3274edaad003d4e3a83
Anne-Dot <ruusmann@gmail.com>
2024-11-27 14:28:07 +0200

    nagu tunnis j6uti

--- app.js
+++ app.js
@@ -1,79 +1,59 @@
-console.log('Javascript is running');
+//näitab sõna, kus tühikud on alakriipsudega asendatud
+const wordDiv = document.getElementById("word");
+const scoreDiv = document.getElementById('score');
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ü', 'õ', 'ö', 'ä'];
+const messageDiv = document.getElementById('message');
+const alphabetDiv = document.getElementById('alphabet');
+let guessedLetters = [];
+let word = "KURESSAARE AMETIKOOL";
+let guess = word.replace(/[a-zA-Z]/g, "_");
+let score = 10;
 
-document.addEventListener('DOMContentLoaded', () => {
-    const wordDiv = document.getElementById('word');
-    const scoreDiv = document.getElementById('score');
-    const messageDiv = document.getElementById('message');
-    const alphabetDiv = document.getElementById('alphabet');
+alphabet.forEach(c => {
+    const letterDiv = document.createElement('div');
+    letterDiv.setAttribute('id', c);
+    letterDiv.classList.add('letter');
+    letterDiv.innerText = c.toUpperCase();
 
+    alphabetDiv.appendChild(letterDiv);
+});
 
-    let score = 10;
+wordDiv.innerText = guess;
+scoreDiv.innerText = score;
 
-    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'õ', 'ä', 'ö', 'ü', 'x', 'y', 'z'];
-    const guessedLetters = [];
-
-    //hiljem sõna API kaudu
-    let word = 'KURESSAARE AMETIKOOL';
-
-
-    // muudab tähed alakriipsudeks
-    let guess = word.replace(/[a-zA-Z]/g, "_");
-
-    alphabet.forEach(c => {
-        const letterDiv = document.createElement('div');
-        letterDiv.setAttribute('id', c);
-        letterDiv.classList.add('letter');
-        letterDiv.innerText = c.toUpperCase();
-
-        alphabetDiv.appendChild(letterDiv);
-    });
-
-
-    wordDiv.innerText = guess;
-    scoreDiv.innerText = score;
-
-    console.log('Script loaded');
-})
-
-
-
-
-//JS kuulab, mis klahvi kasutaja vajutab, saab näha Inspector -> Console all kui vajutatud ja töötab
-document.addEventListener('keydown', e => {
+//kuulab klaviatuuri sisendeid
+document.addEventListener("keydown", e => {
     let letter = e.key;
+    if (alphabet.includes(letter) && !guessedLetters.includes(letter) && score != 0) { //kui tahed on 0 siis rohkem ei saa lisada
 
-    if (alphabet.includes(letter) && !guessedLetters.includes(letter) && score != 0) {
-
-        guessedLetters.push(letter);
+        guessedLetters.push(letter); //ei lase vale mitu korda vajutada
+        const guessedLetterDiv = document.getElementById(letter);
 
         if (word.includes(letter.toUpperCase())) {
 
-            // i on tähe indeks
             let i = -1;
-            let guessArray = guess.split('');
-            // do while teeb enne ühe tingimuse, siis kontrollib
+            let guessArray = guess.split('')
             do {
                 i = word.indexOf(letter.toUpperCase(), i + 1);
                 guessArray[i] = letter.toUpperCase();
+                console.log(i);
             } while (i != -1);
             guess = guessArray.join('');
-            console.log(guess);
             wordDiv.innerText = guess;
 
             if (!guess.includes('_')) {
-                messageDiv.innerText = 'Arvasid ära!';
+                messageDiv.innerText = "You won!"
             }
+            guessedLetterDiv.classList.add('correct-letter');
 
-            // console.log(word.matchAll('A'));
-            // //guess = guess.replace(letter, );
-            // console.log(letter);
         } else {
             score--;
             scoreDiv.innerText = score;
 
             if (!score) {
-                messageDiv.innerText = 'Mäng läbi!';
+                messageDiv.innerText = "Game over!"
             }
+            guessedLetterDiv.classList.add('wrong-letter');
         }
-    };
+    }
 });
