0e2a26f45734f5be8498f7632fa317c72769919e
Anne-Dot <ruusmann@gmail.com>
2024-11-28 11:49:12 +0200

    ussi pea ja toit on ekraanil - algus vaade

--- ussim2ngGame/app.js
+++ ussim2ngGame/app.js
@@ -1,24 +1,53 @@
 const gameBoardTable = document.getElementById('game-board');
 
 const height = 20;
-const width = 20;
+const width = 30;
+
+let foodY, foodX;
+let snake = initSnake();
+
+generateFood();
+drawGameboard();
+
+function generateFood() {
+    foodY = Math.floor(Math.random() * height);
+    foodX = Math.floor(Math.random() * width);
+}
+
+function initSnake() {
+    const snakeY = Math.floor(height / 2);
+    const snakeX = Math.floor(width / 2);
+
+    return [snakeY + '_' + snakeX];
+}
 
 // ühendab HTML-i ID-ga
-// const dataYTd = document.getElementById('data-y')
+function drawGameboard() {
 
-for (let y = 0; y < height; y++) {
-    // console.log(y);
-    const tr = document.createElement('tr')
+    for (let y = 0; y < height; y++) {
+        // console.log(y);
+        const tr = document.createElement('tr')
 
-    for (let x = 0; x < width; x++) {
+        for (let x = 0; x < width; x++) {
 
-        const td = document.createElement('td')
+            const td = document.createElement('td')
 
-        td.dataset.y = y;
-        td.dataset.x = x;
+            td.dataset.y = y;
+            td.dataset.x = x;
 
-        tr.appendChild(td);
+            // kontrollima iga korral
+            if (y == foodY && x == foodX) {
+                td.innerText = '🥦';
+            }
+
+            if (snake.includes(y + '_' + x)) {
+                td.innerText = '😈';
+            }
+
+            tr.appendChild(td);
+        }
+
+        gameBoardTable.appendChild(tr);
     }
-
-    gameBoardTable.appendChild(tr);
 }
+
