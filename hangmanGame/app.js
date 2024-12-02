e8b1da50331032dffa80b185952d4ce0e80a82a6
Anne-Dot <ruusmann@gmail.com>
2024-12-02 15:50:39 +0200

    images added and message moved lower on screen

--- hangmanGame/app.js
+++ hangmanGame/app.js
@@ -3,12 +3,32 @@ const wordDiv = document.getElementById('word');
 const scoreDiv = document.getElementById('score');
 const messageDiv = document.getElementById('message');
 const alphabetDiv = document.getElementById('alphabet');
+const hangmanImageDiv = document.getElementById('hangmanImage')
 
 
 //arvamiste kord
 let score = 10;
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'õ', 'ä', 'ö', 'ü', 'x', 'y', 'z'];
 let guessedLetters = [];
+
+// poomismängu pildid
+const hangmanVisuals = [
+    "images/stage00.png",
+    "images/stage01.png",
+    "images/stage02.png",
+    "images/stage03.png",
+    "images/stage04.png",
+    "images/stage05.png",
+    "images/stage06.png",
+    "images/stage07.png",
+    "images/stage08.png",
+    "images/stage09.png",
+    "images/stage10.png"
+]
+// lae algne pilt
+let imageIndex = 0;
+
+// hangman = hangmanImageDiv.innerText(hangmanVisuals[0]);
 
 //hiljem sõna API kaudu
 let word = await getRandomWord();
@@ -23,13 +43,13 @@ alphabet.forEach(c => {
     const letterDiv = document.createElement('div');
     letterDiv.setAttribute('id', c);
     letterDiv.classList.add('letter');
-    letterDiv.innerText= c.toUpperCase();
+    letterDiv.innerText = c.toUpperCase();
 
-    letterDiv.addEventListener('click', e =>{
+    letterDiv.addEventListener('click', e => {
         testLetter(c);
     }),
 
-    alphabetDiv.appendChild(letterDiv);
+        alphabetDiv.appendChild(letterDiv);
 });
 
 
@@ -39,22 +59,22 @@ scoreDiv.innerText = score;
 
 
 //JS kuulab, mis klahvi kasutaja vajutab, saab näha Inspector -> Console all kui vajutatud ja töötab
-document.addEventListener('keydown', e=> {
+document.addEventListener('keydown', e => {
     testLetter(e.key);
 });
 
-function testLetter(letter){
+function testLetter(letter) {
 
 
-   
+
     // võib ära jätta score !=0, sest score peab olema true ja 0 on false
-    if(alphabet.includes(letter)&& !guessedLetters.includes(letter) && score != 0){
-        
-        guessedLetters.push(letter);
+    if (alphabet.includes(letter) && !guessedLetters.includes(letter) && score != 0) {
 
+        guessedLetters.push(letter);
+        
         const guessedLetterDiv = document.getElementById(letter);
 
-        if(word.includes(letter.toUpperCase())){
+        if (word.includes(letter.toUpperCase())) {
 
             // i on tähe indeks
             let i = -1;
@@ -62,15 +82,15 @@ function testLetter(letter){
 
             // do while teeb enne ühe tingimuse, siis kontrollib
             do {
-              i = word.indexOf(letter.toUpperCase(),i + 1);  
-              guessArray[i] = letter.toUpperCase();
-              console.log(i);
-            } while (i !=-1);
+                i = word.indexOf(letter.toUpperCase(), i + 1);
+                guessArray[i] = letter.toUpperCase();
+                console.log(i);
+            } while (i != -1);
             guess = guessArray.join('');
             console.log(guess);
             wordDiv.innerText = guess;
             //kui sõnas pole enam alakriipse
-            if(!guess.includes('_')){
+            if (!guess.includes('_')) {
                 messageDiv.innerText = "Arvasid ära!";
 
             }
@@ -80,7 +100,10 @@ function testLetter(letter){
             score--;
             scoreDiv.innerText = score;
 
-            if ( !score ){
+            imageIndex++;
+            hangmanImageDiv.src = hangmanVisuals[imageIndex];
+
+            if (!score) {
                 messageDiv.innerText = "Mäng läbi! \n Õige sõna oli " + word;
             }
 
@@ -90,17 +113,16 @@ function testLetter(letter){
     };
 };
 
-async function getRandomWord(){
+async function getRandomWord() {
     const response = await fetch('hangman.txt');
     let words = await response.text();
 
     words = words.split('\n');
-    const word = words[Math.floor(Math.random()*words.length)];
-        
-    
-    
-    return word.toUpperCase();
-    
-}
+    const word = words[Math.floor(Math.random() * words.length)];
 
+
+
+    return word.toUpperCase();
+
+}
 
